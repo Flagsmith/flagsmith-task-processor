93b141e2b78f9fb45bdecdbc8207413b5563c7c7
Matthew Elwell <matthew.elwell@flagsmith.com>
2024-01-30 11:55:20 +0000

    Add typing to managers module

--- task_processor/managers.py
+++ task_processor/managers.py
@@ -2,10 +2,10 @@ from django.db.models import Manager
 
 
 class TaskManager(Manager):
-    def get_tasks_to_process(self, num_tasks):
+    def get_tasks_to_process(self, num_tasks: int):
         return self.raw("SELECT * FROM get_tasks_to_process(%s)", [num_tasks])
 
 
 class RecurringTaskManager(Manager):
-    def get_tasks_to_process(self, num_tasks):
+    def get_tasks_to_process(self, num_tasks: int):
         return self.raw("SELECT * FROM get_recurringtasks_to_process(%s)", [num_tasks])
