52fb3160946f2dbffe0da7e386891e0e092107ee
Matthew Elwell <matthew.elwell@flagsmith.com>
2024-01-30 12:05:36 +0000

    Refactor helpers

--- task_processor/migrations/helpers/__init__.py
+++ task_processor/migrations/helpers/__init__.py
@@ -1,29 +1,9 @@
-import os
-import typing
-from contextlib import suppress
+"""
+Note: django doesn't support adding submodules to the migrations module directory
+that don't include a Migration class. As such, I've defined this helpers submodule
+and simplified the imports by defining the __all__ attribute.
+"""
 
-from django.db import migrations
+from task_processor.migrations.helpers.postgres_helpers import PostgresOnlyRunSQL
 
-
-class PostgresOnlyRunSQL(migrations.RunSQL):
-    @classmethod
-    def from_sql_file(
-        cls,
-        file_path: typing.Union[str, os.PathLike],
-        reverse_sql: typing.Union[str, os.PathLike] = None,
-    ) -> "PostgresOnlyRunSQL":
-        with open(file_path) as forward_sql:
-            with suppress(FileNotFoundError, TypeError):
-                with open(reverse_sql) as reverse_sql_file:
-                    reverse_sql = reverse_sql_file.read()
-            return cls(forward_sql.read(), reverse_sql=reverse_sql)
-
-    def database_forwards(self, app_label, schema_editor, from_state, to_state):
-        if schema_editor.connection.vendor != "postgresql":
-            return
-        super().database_forwards(app_label, schema_editor, from_state, to_state)
-
-    def database_backwards(self, app_label, schema_editor, from_state, to_state):
-        if schema_editor.connection.vendor != "postgresql":
-            return
-        super().database_backwards(app_label, schema_editor, from_state, to_state)
+__all__ = ["PostgresOnlyRunSQL"]
