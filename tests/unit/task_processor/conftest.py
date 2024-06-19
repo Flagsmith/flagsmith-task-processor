d401c7d2b14ca31cdf5b078462f4dbe8d99eba4a
Matthew Elwell <matthew.elwell@flagsmith.com>
2024-06-19 08:14:10 +0100

    Formatting fix

--- tests/unit/task_processor/conftest.py
+++ tests/unit/task_processor/conftest.py
@@ -10,9 +10,8 @@ def run_by_processor(monkeypatch):
 
 
 class GetTaskProcessorCaplog(typing.Protocol):
-    def __call__(
-        self, log_level: str | int = logging.INFO
-    ) -> pytest.LogCaptureFixture: ...
+    def __call__(self, log_level: str | int = logging.INFO) -> pytest.LogCaptureFixture:
+        ...
 
 
 @pytest.fixture
